e20adaefef4a87f837fec52750def2a8e93d2d6b
Antoine Polo <95935692+Antoinepolo@users.noreply.github.com>
2026-02-20 14:43:35 +0100

    Refactor city population display logic
    Removed total population calculations and outputs for French and Dutch cities. Added condition to display cities with populations over 500,000.

--- Sécurité Web/Laboratoire N°1/Exo6.1.1.b.php
+++ Sécurité Web/Laboratoire N°1/Exo6.1.1.b.php
@@ -6,16 +6,8 @@
         $villes["lg"] = array("Liege", 200000, "Liege", "fr");
         $villes["gand"] = array("Gent", 260000, "Oost−Vlaanderen", "nl");
 
-        $total_fr = 0;
-        $total_nl = 0;
-
         foreach ($villes as $ville) {
-                if ($ville[3] == "fr")
-                        $total_fr += $ville[1];
-                else
-                        $total_nl += $ville[1];
+                if ($ville[1] > 500000)
+                        echo $ville[0] . ", " . $ville[1] . " habitants <br>";
         }
-
-        echo "Total FR : " . $total_fr . "<br>";
-        echo "Total NL : " . $total_nl . "<br>";
 ?>
