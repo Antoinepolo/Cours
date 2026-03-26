119b51bb87cffb0d20c87c35835addd58c1d6136
Antoine Polo <95935692+Antoinepolo@users.noreply.github.com>
2026-03-26 17:45:57 +0100

    Add login functionality with session management

---
+++ Sécurité Web/Laboratoire N°4/login.php
@@ -0,0 +1,65 @@
+<?php
+// On Charge l'autoloader, ça évite d'inclure tout les fichiers à la main
+require 'vendor/autoload.php';
+
+// On indique des racourcis de classes
+use Root\Html\Connection;
+use Dotenv\Dotenv;
+
+// On initialise le fichier .env qui contient les credentials de connexion à la DB, ça évite des les mettres en clair dans le programme
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// On se connecte à la base de donnée
+$pdo = Connection::make(
+    $_ENV['DATABASE_HOST'],
+    $_ENV['DATABASE_NAME'],
+    $_ENV['DATABASE_USER'],
+    $_ENV['DATABASE_PASSWORD']
+);
+
+// Variable récupérée dans le formulaire
+$username = $_POST['username'] ?? null;
+$password = $_POST['password'] ?? null; 
+
+// On créer une nouvelle session avec un ID ou la recharger si elle existe
+session_start();
+
+try {
+    // On prépare la requête
+    $sql = "SELECT nom, prenom, password FROM users WHERE nom = :username";
+    // On prépare l'envoie à la base donnée
+    $stmt = $pdo->prepare($sql);
+
+    // On exécute la requête
+    $stmt->execute([':username' => $username]);
+
+    // On récupère la ligne correspondante
+    $user = $stmt->fetch(); // Récupère la première ligne correspondante
+
+    // Si on a un utilisateur
+    if ($user) {
+        // Si le mot de passe est bon
+        if (password_verify($password, $user['password'])) {
+            // On enregistres les credentials et le status de connexion dans la superglobale session
+            $_SESSION['prenom'] = $user['prenom'];
+            $_SESSION['nom'] = $user['nom'];
+
+            // On redirige l'utilisateur vers home.php
+            header('Location: home.php');
+
+            // On termine le script de login
+            exit();
+        } else {
+            // Sinon (mot de passe incorrect), on affiche une erreur
+            echo "Mot de passe incorrect";
+        }
+    } else {
+        // Si aucun utilisateur n'a été trouver, on l'indique
+        echo "Utilisateur non enregistré.";
+    }
+
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
